ebfd797072a1630a74532bdf71f80828be4788eb
EmmaExcel <excelemma6@gmail.com>
2024-05-03 06:47:42 -1100

    added some changes to script

---
+++ dist/dist/assets/ButtonPrimary-DfJr3lRy.js
@@ -0,0 +1,35 @@
+import{s as r,c as o,j as t,B as d}from"./index-GxelU0FC.js";const n=r(d)`
+    background-color: ${o.mint40};
+    border-color: ${o.mint20};
+    color: ${o.white};
+
+    :hover {
+        background: radial-gradient(
+                71.63% 130.21% at 50% 0%,
+                #aadcd6 0%,
+                rgba(33, 191, 150, 0) 100%
+            ),
+            #21bf96;
+    }
+
+    :active {
+        border-color: ${o.mint20};
+        background: linear-gradient(
+                83.64deg,
+                #aadcd6 -9.46%,
+                rgba(33, 191, 150, 0) 45.97%,
+                #aadcd6 103.7%
+            ),
+            #21bf96;
+        outline: 0;
+        box-shadow: none;
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.navy40};
+    }
+
+    svg {
+        fill: ${o.white};
+    }
+`;var e={ButtonPrimaryStyled:n};const{ButtonPrimaryStyled:s}=e,i=({...a})=>t(s,{...a});export{i as default};
