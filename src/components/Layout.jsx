0e6a338ddef8922471ce9ff5ce6cdad9c8515075
EmmaExcel <excelemma6@gmail.com>
2024-05-08 00:34:58 -1100

    login working

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,30 +7,30 @@ import { AgentOfferContextProvider } from "../context/AgentOffer.context";
 import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider, useSession } from "next-auth/react";
 import Script from "next/script";
-
+import LoginProvider from "../context/Login.context";
 
 const Layout = ({ children }) => {
-
   const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-  
+
   //   const { data: session } = useSession();
   return (
     <SessionProvider>
       <div className="font-poppins">
-         
-         <Script
-  defer
-  src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initmap`} 
-></Script>
-   
-        <RegistrationContextProvider>
-          <Navbar />
-          <AgentOfferContextProvider>
-            {children}
-            <Analytics />
-          </AgentOfferContextProvider>
-          <Footer />
-        </RegistrationContextProvider>
+        <Script
+          defer
+          src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initmap`}
+        ></Script>
+
+        <LoginProvider>
+          <RegistrationContextProvider>
+            <Navbar />
+            <AgentOfferContextProvider>
+              {children}
+              <Analytics />
+            </AgentOfferContextProvider>
+            <Footer />
+          </RegistrationContextProvider>
+        </LoginProvider>
       </div>
     </SessionProvider>
   );
