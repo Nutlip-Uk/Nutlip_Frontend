b5f4532a41146389137841e2ecfb108f6424b390
EmmaExcel <excelemma6@gmail.com>
2024-06-03 11:04:51 -1100

    added enivronment variable for firebase.js

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5WNYsDt1rflQgezH133LrcYxd7PvLdb4",
-  authDomain: "nutlipimages.firebaseapp.com",
-  projectId: "nutlipimages",
-  storageBucket: "nutlipimages.appspot.com",
-  messagingSenderId: "223737850307",
-  appId: "1:223737850307:web:8001ef4f78d3afd5f77576",
-  measurementId: "G-DDFWDY8JS6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain:process.env.FIREBASE_AUTHDOMAIN ,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
