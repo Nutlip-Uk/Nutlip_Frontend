27b73726dbd2b45ba065ee30ae1e2814e54fbe63
EmmaExcel <excelemma6@gmail.com>
2024-06-22 11:38:47 -1100

    make offer integration

--- src/context/MakeAnOffer.context.jsx
+++ src/context/MakeAnOffer.context.jsx
@@ -1,25 +1,59 @@
-import { createContext, useState } from "react";
+import { createContext, useState,useEffect } from "react";
 
 export const MakeAnOffer = createContext(null)
 
 export const MakeAnOfferProvider = ({ children }) => {
+  useEffect(() => {
+    // Retrieve the userInformation from the local storage
+    const userInformation = JSON.parse(localStorage.getItem("userInformation"));
+    console.log("userInformation:", userInformation);
+
+    if (userInformation && userInformation.user) {
+      // Extract the userId from the userInformation object
+      const userId = userInformation.user.id;
+      console.log("userId:", userId);
+      if (userId) {
+        setForm((prevForm) => ({ ...prevForm, userId }));
+      }
+    }
+  }, []);
     const [form, setForm] = useState({
-        fullName:"",
+      apartmentId:"",
+      userId: "",
+        FullName:"",
         Address:"",
-        interest:"",
-        priceOffer:"",
+        Interested:"",
+        PriceOffer:"",
         NutlipCommission:"",
-        AgentSellerReceives:"",
-        PaymentMethod:"",
+        receivedPayment:"",
+        PaymentType:"",
         cryptoType:""
       });
 
       const handleChange = (e) => {
-        setForm({
-          ...form,
-          [e.target.name]: e.target.value
-        })
-      }
+        const { name, value } = e.target;
+      
+        if (name === "PriceOffer") {
+          const nutlipCommission = value * 0.005;
+          const receivedPayment = value - nutlipCommission;
+          setForm({
+            ...form,
+            [name]: value,
+            NutlipCommission: nutlipCommission.toFixed(2),
+            receivedPayment: receivedPayment.toFixed(2)
+          });
+        } else if (name === "Interested") {
+          setForm({
+            ...form,
+            [name]: value === "Yes" ? true : false,
+          });
+        } else {
+          setForm({
+            ...form,
+            [name]: value,
+          });
+        }
+      };
 
     return (
         <MakeAnOffer.Provider value={{form , handleChange}}>
--- src/models/Offers.js
+++ src/models/Offers.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const offerSchema = new mongoose.Schema({
-  offerPrice: {
-    type: Number,
-    required: true,
-  },
   apartmentId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
