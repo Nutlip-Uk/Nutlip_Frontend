bdbe0bfd74e84155f8722dbac82c3ccfeca0cf21
EmmaExcel <excelemma6@gmail.com>
2024-07-06 23:43:20 -1100

    post property

--- src/pages/api/apartments/index.js
+++ src/pages/api/apartments/index.js
@@ -2,7 +2,6 @@
 
 import dbConnect from "../../../libs/dbconnect";
 import Apartment from "../../../models/Apartment";
-import User from "../../../models/User";
 
 export default async function handler(req, res) {
   await dbConnect();
@@ -29,7 +28,7 @@ export default async function handler(req, res) {
       bathrooms,
       Toilets,
       size,
-      livingRoom,
+      LivingRoom,
       PCW,
       PCM,
       stateOfProperty,
@@ -63,7 +62,7 @@ export default async function handler(req, res) {
         bathrooms,
         Toilets,
         size,
-        livingRoom,
+        LivingRoom,
         PCW,
         PCM,
         stateOfProperty,
