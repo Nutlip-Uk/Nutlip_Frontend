838151b0960829e0198bd0f86f3dc8f6dbdbea50
Mac <user@Excel-MacBook-Pro.local>
2024-09-25 20:45:16 +0100

    chan

--- src/context/MakeAnOffer.context.jsx
+++ src/context/MakeAnOffer.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState,useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const MakeAnOffer = createContext(null)
 
@@ -17,47 +17,47 @@ export const MakeAnOfferProvider = ({ children }) => {
       }
     }
   }, []);
-    const [form, setForm] = useState({
-      apartmentId:"",
-      userId: "",
-        FullName:"",
-        Address:"",
-        Interested:"",
-        PriceOffer:"",
-        NutlipCommission:"",
-        receivedPayment:"",
-        PaymentType:"",
-        cryptoType:""
+  const [form, setForm] = useState({
+    apartmentId: "",
+    userId: "",
+    FullName: "",
+    Address: "",
+    Interested: "",
+    PriceOffer: "",
+    NutlipCommission: "",
+    receivedPayment: "",
+    PaymentType: "",
+    cryptoType: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "PriceOffer") {
+      const nutlipCommission = value * 0.005;
+      const receivedPayment = value - nutlipCommission;
+      setForm({
+        ...form,
+        [name]: value,
+        NutlipCommission: nutlipCommission.toFixed(2),
+        receivedPayment: receivedPayment.toFixed(2)
       });
+    } else if (name === "Interested") {
+      setForm({
+        ...form,
+        [name]: value === "Yes" ? true : false,
+      });
+    } else {
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    }
+  };
 
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-      
-        if (name === "PriceOffer") {
-          const nutlipCommission = value * 0.005;
-          const receivedPayment = value - nutlipCommission;
-          setForm({
-            ...form,
-            [name]: value,
-            NutlipCommission: nutlipCommission.toFixed(2),
-            receivedPayment: receivedPayment.toFixed(2)
-          });
-        } else if (name === "Interested") {
-          setForm({
-            ...form,
-            [name]: value === "Yes" ? true : false,
-          });
-        } else {
-          setForm({
-            ...form,
-            [name]: value,
-          });
-        }
-      };
-
-    return (
-        <MakeAnOffer.Provider value={{form , handleChange}}>
-        {children}
-        </MakeAnOffer.Provider>
-    )
+  return (
+    <MakeAnOffer.Provider value={{ form, handleChange }}>
+      {children}
+    </MakeAnOffer.Provider>
+  )
 }
