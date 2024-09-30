481b40fc6e23ee2c3182fb4a2fd17712a24e3b86
Emma Excel <excelemma6@gmail.com>
2024-09-30 14:27:39 +0100

    changes

--- src/context/ImageContext.context.jsx
+++ src/context/ImageContext.context.jsx
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import React, { createContext, useState, useContext } from 'react';
-=======
-import React, { createContext, useState } from 'react';
->>>>>>> 3a30097087fe14f9e156140d83b0807a172c1731
 
 export const ImageContext = createContext();
 
 export const ImageProvider = ({ children }) => {
   const [url, setUrl] = useState([]);
-<<<<<<< HEAD
   const [viewOptions, setViewOptions] = useState("")
   const [loading, setLoading] = useState(false);
 
@@ -20,14 +15,4 @@ export const ImageProvider = ({ children }) => {
 };
 
 export const useImageContext = () => useContext(ImageContext);
-=======
-  const [viewOptions , setViewOptions]=useState("")
- 
-
-  return (
-    <ImageContext.Provider value={{ url, setUrl, viewOptions,setViewOptions }}>
-      {children}
-    </ImageContext.Provider>
-  );
-};
->>>>>>> 3a30097087fe14f9e156140d83b0807a172c1731
+e
